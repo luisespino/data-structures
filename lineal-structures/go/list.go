4cdea4123161c9544e90d8a2207de5735576af4b
Luis Espino <66042898+luisespino@users.noreply.github.com>
2021-02-05 02:10:02 -0600

    Create list.go

---
+++ lineal-structures/go/list.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	data int
+	next *Node
+}
+
+type List struct {
+	head *Node
+}
+
+func (l *List) add(data int) {
+	tmp := &Node{data: data, next: l.head}
+	l.head = tmp
+}
+
+func (l *List) show() {
+	tmp := l.head
+	for tmp != nil {
+		fmt.Print(tmp.data, " ")
+		tmp = tmp.next
+	}
+}
+
+func main() {
+	l := List{}
+	l.add(3)
+	l.add(9)
+	l.add(6)
+	l.show()
+}
