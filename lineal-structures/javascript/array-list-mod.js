305bf32fbcc9bc38f5394b21e54bbc8c09d236b7
Luis Espino <66042898+luisespino@users.noreply.github.com>
2021-06-07 00:58:01 -0600

    upload arraylistmod

---
+++ lineal-structures/javascript/array-list-mod.js
@@ -0,0 +1,45 @@
+class Nodo {
+  constructor(valor) {
+    this.valor = valor
+    this.siguiente = null
+  }
+}
+
+class Lista{
+  constructor() {
+    this.cabeza = null
+  }
+  
+  agregar(valor) {
+    var temp = new Nodo(valor)
+    temp.siguiente = this.cabeza
+    this.cabeza = temp
+  }
+  
+  mostrar() {
+    var temp = this.cabeza
+    document.getElementById("log").innerHTML+="<br>[ "    
+    while(temp) {
+      document.getElementById("log").innerHTML+=temp.valor+" ";
+      temp = temp.siguiente
+    }
+    document.getElementById("log").innerHTML+="]"
+  } 
+}
+
+var a = []
+
+for (var i = 0; i < 5; i++) {
+a[i] = new Lista()
+}
+
+a[1%5].agregar(1)
+a[3%5].agregar(3)
+a[6%5].agregar(6)
+a[8%5].agregar(8)
+a[14%5].agregar(14)
+
+for (var i = 0; i < 5; i++) {
+document.getElementById("log").innerHTML+="<br>"+i+" -> "  
+a[i].mostrar()
+}
