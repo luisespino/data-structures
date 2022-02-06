b36fd959d0e2d9989f6ecd4839bf14d5745ad583
Luis Espino <66042898+luisespino@users.noreply.github.com>
2022-02-05 21:51:01 -0600

    Create LinkedList.cpp

---
+++ cpp/lineal-structures/LinkedList.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+class Node
+{
+public:
+    int data;
+    Node* next;
+    Node (int data_) : data(data_), next(0) {}
+};
+
+class LinkedList
+{
+public:
+    Node* head = 0;
+    void addStart(int data)
+    {
+        Node* tmp = new Node(data);
+        tmp->next = head;
+        head = tmp;
+    }
+
+    void show()
+    {
+        Node* tmp = head;
+        while (tmp != 0)
+        {
+            std::cout << tmp->data << std::endl;
+            tmp = tmp->next;
+        }
+    }
+};
+
+int main()
+{
+    LinkedList *list1 = new LinkedList();
+    list1->addStart(6);
+    list1->addStart(10);
+    list1->addStart(7);
+    list1->show();
+    return 0;
+}
