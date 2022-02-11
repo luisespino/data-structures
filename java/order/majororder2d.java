82b663748035dae028187925cb399f315763b3da
Luis Espino <66042898+luisespino@users.noreply.github.com>
2022-02-11 14:38:11 -0600

    Create majororder2d.java

---
+++ java/order/majororder2d.java
@@ -0,0 +1,23 @@
+public class majororder2d {
+
+    public static void main(String [] args) {
+        int [][] m = {{0, 1, 2, 3}, {4, 5, 6, 7}, {8, 9, 10, 11}, {12, 13, 14, 15}};
+        int [] rowmajor = new int[16];
+        int [] colmajor = new int[16];
+    
+        for (int i = 0; i < 4; i++)
+            for (int j = 0; j < 4; j++){
+                rowmajor[i*4+j] = m[i][j];
+                colmajor[i+4*j] = m[i][j];
+    
+            }
+    
+        for (int i = 0; i < 16; i++)
+            System.out.print (rowmajor[i]+" ");
+        System.out.println();
+        for (int i = 0; i < 16; i++)
+            System.out.print (colmajor[i]+" ");
+
+    }
+
+}
