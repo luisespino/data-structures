56e6a7fc9019bd5f21bb9f4ec1e3c2f17db4d97a
Luis Espino <66042898+luisespino@users.noreply.github.com>
2022-02-11 17:39:21 -0600

    Update majororder2d.java

--- java/algorithms/majororder2d.java
+++ java/algorithms/majororder2d.java
@@ -1,5 +1,5 @@
 
-public class majororder2d {
+public class MajorOrder2D {
 
     public static void main(String [] args) {
         int [][] m = {{0, 1, 2, 3}, {4, 5, 6, 7}, {8, 9, 10, 11}, {12, 13, 14, 15}};
